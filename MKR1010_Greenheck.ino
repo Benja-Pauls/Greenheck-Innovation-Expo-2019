761fc1d190194a65e7668b88aa7c529b374131b2
Ben Paulson <73416124+Benja-Pauls@users.noreply.github.com>
2021-11-28 16:37:54 -0600

    Update MKR1010_Greenheck.ino

--- MKR1010_Greenheck.ino
+++ MKR1010_Greenheck.ino
@@ -1,17 +1,42 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+
 #include <SPI.h>
 #include <WiFiNINA.h>
 
-//Our pin #'s assigned to a variable
-const int x = 3;
+//variables
+int b_power_v = 0;
+int b_heat_v = 0;
+int s_lock_v = 0;
+int s_lock_v2 = 0; // virtual lock
+int filter = 0;
+int x = 0;
+int y = 0;
+int z = 0;
+int x2 = 0;
+int y2 = 0;
+int z2 = 0;
+int x_c = 0;
+int x_s = 0;
+int x_ls = 0;
+int y_c = 0;
+int y_s = 0;
+int y_ls = 0;
 
-//Our changed variables
-int x_counter = 0;
-int x_state = 0;
-int x_last_state = 0;
+
+
+//relay
+int r_power = A1;
+int r_heat = A0;
+
+//buttons and switches
+int p_b_power = 3;
+int p_b_heat = 4;
+int p_s_lock = 5;
 
 char ssid[] = "TDSV1000W1280";      //network's id
 char pass[] = "PT5GTTJT3NKY6N6Z";   //network's password
-int keyIndex = 0;                   //is Greenheck WEP?
+int keyIndex = 0;                   
 
 int status = WL_IDLE_STATUS;
 WiFiServer server{80};              //80 is for wifi, not ethernet
@@ -21,20 +46,19 @@ char quote = '"';
 char slash = '/';
 
 void setup() {
+  Serial.begin(9600);
+  
+  lcd.begin(16,2);
 
-Serial.begin(9600);
 
-  //DIGITAL PINS FOR LED
-  pinMode(1, OUTPUT);
-  pinMode(0, OUTPUT);
-  pinMode(6, OUTPUT);
+pinMode(A1,OUTPUT);
+pinMode(A0, OUTPUT);
 
-  //Read Values
-  pinMode(x, INPUT_PULLUP); 
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
+  pinMode(p_b_power, INPUT_PULLUP);
+  pinMode(p_b_heat, INPUT_PULLUP);
+  pinMode(p_s_lock, INPUT_PULLUP);
+
+
 
   //check for shield
   if (WiFi.status() == WL_NO_SHIELD)  {
@@ -43,6 +67,7 @@ Serial.begin(9600);
   }
 
   //attempt to connect to the network
+
   while ( status != WL_CONNECTED) {
     Serial.println("Attempting to connect to Network named: ");
     Serial.println(ssid);     //printing the network name
@@ -52,35 +77,59 @@ Serial.begin(9600);
     //wait 10 seconds for connection:
     delay(10000);
   }
-  server.begin();         //start the web server 
+  server.begin();   
+  printWiFiStatus();//start the web server
 }
 
-
 void loop() {
-
-  x_state = digitalRead(x);
-
-  //compare states (display button press count when pressed)
-  if (x_state != x_last_state) {
-    if(x_state == 0) {
-      x_counter++;
-      Serial.println("on");
-      Serial.print("The button has been pressed: ");
-      Serial.println(x_counter);
-      Serial.print(" times");
-    }else {
-      Serial.println("off");
+  x_s = digitalRead(p_b_power);
+  s_lock_v2 = 0;
+ 
+//power
+    if(x_s == 0){
+      x_c++;
+      delay(1000);
     }
     delay(50);
-  }
-  x_last_state = x_state;
+    
+ if(x_c == 1){
+  if(s_lock_v2 == 0) digitalWrite(r_power, HIGH);
+ }
+ else digitalWrite(r_power, LOW);
+ if(x_c == 2){
+  x_c--;
+  x_c--;
+ }
 
-  if (x_counter %2 == 0) {
-    Serial.print("OFF");
-  }else {
-    Serial.print("ON");
-  }
-  
+
+//heat
+  y_s = digitalRead(p_b_heat);
+
+ 
+  //compare
+    if(y_s == 0)
+      if(x_c == 1){
+      y_c++;
+      delay(1000);
+    }
+    delay(50);
+    
+ if(y_c == 1){
+  if(s_lock_v2 == 0) digitalWrite(r_heat, HIGH);
+ }
+ else digitalWrite(r_heat, LOW);
+ if(y_c == 2){
+  y_c--;
+  y_c--;
+ }
+
+s_lock_v = digitalRead(p_s_lock);
+
+lcd.setCursor(0,0);
+if(s_lock_v2 == 1) 
+lcd.print("Lock = ON");
+else lcd.print("Lock = OFF");
+
 
 
   //attempt to connect to WiFi network:
@@ -164,7 +213,7 @@ void loop() {
             client.print("Digital Input ");
             client.print("3");
             client.print(" is ");
-            client.print(x_last_state);
+            client.print(sensorReading);
             client.println("<br />"); }
 
             client.println("<html>");
@@ -223,36 +272,46 @@ void loop() {
 
         //Pin outputs for POWER
         if (currentLine.endsWith("GET /O"))  {
-          digitalWrite(6, HIGH);                 //Get /O turns 7 on
-          }
+          if (s_lock_v == 0) {
+           x_c++;
+           delay(1000);
+          }                             //Get /O turns 3 on
+        }
         if (currentLine.endsWith("GET /F"))  {
-          digitalWrite(6, LOW);                  //Get /F turns 7 off
+          if(s_lock_v == 0){
+          if(x_c == 1)
+          x_c++;                        //Get /F turns 3 off
+          delay(1000);
           }
+        }
 
         //Pin outputs for HEAT
         if (currentLine.endsWith("GET /H"))  {
-          digitalWrite(0, HIGH);                //Get /H turns 6 on
-          }
+          if (s_lock_v == 0) {
+            if(x_c == 1) y_c++;
+          }                             //Get /H turns 4 on
+        }
         if (currentLine.endsWith("GET /C"))  {
-          digitalWrite(0, LOW);                 //Get /C turns 6 off
-          }
+         if(s_lock_v == 0)
+         if(y_c == 1) y_c++;            //Get /C turns 4 off
+        }
 
         //Pin outputs for LOCK
         if (currentLine.endsWith("GET /L"))  {
-          digitalWrite(1, HIGH);                //Get /L turns 5 on
+          s_lock_v2++;
+          //Get /L turns 5 on
         }
         if (currentLine.endsWith("GET /U"))  {
-          digitalWrite(1, LOW);                 //Get /U turns 5 off
+          s_lock_v2--;                //Get /U turns 5 off
         }
 
-        }
       }
-      //close the connection:
-      client.stop();
-      Serial.println("client disconnected");
     }
+    //close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
 }
-
 void printWiFiStatus()  {
   //print the SSID of the network you're attached to:
   Serial.print("SSID: ");
@@ -272,5 +331,4 @@ void printWiFiStatus()  {
   //print where to go in browser:
   Serial.print("To see this page in action, open a browser to http://");
   Serial.println(ip);
-
-} 
+}
